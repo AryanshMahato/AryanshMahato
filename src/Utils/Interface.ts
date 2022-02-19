ae27eff33a815fe5689837654fd35fe1b36ccdc8
Aryansh Mahato <aryanshmahato@gmail.com>
2022-02-19 21:11:52 +0530

    remove unnecessary legacy code

--- src/Utils/Interface.ts
+++
@@ -1,7 +0,0 @@
-import { IconBaseProps, IconType } from "react-icons";
-
-export interface SocialHandle {
-  name: string;
-  logo: IconType | IconBaseProps;
-  link: string;
-}
--- src/Utils/enums.ts
+++
@@ -1,6 +0,0 @@
-enum NavItemType {
-  normal,
-  cta
-}
-
-export { NavItemType };
